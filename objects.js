3bf568de5da736a7ba246f8609826976a6bda043
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-27 03:43:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,3 @@
-const recipe = { eggs: 3 }
-
- updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup')
-console.log(recipe)
-
-// returns `{ eggs: 3, chocolate: '1 cup' }`
- 
-recipe // { eggs: 3 }
-
 function updateObjectWithKeyAndValue(obj, key, value) {
  
   return Object.assign({}, obj, { [key]: value })
@@ -17,3 +8,9 @@ function updateObjectWithKeyAndValue(obj, key, value) {
     // Test what happens if this line was written as:
     // return Object.assign(obj, { [key]: value })
  
+const recipe = { eggs: 3 }
+ 
+updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup')
+// returns `{ eggs: 3, chocolate: '1 cup' }`
+ 
+recipe // { eggs: 3 }
