9f8d9609949a7326608a912513d2199d83773dff
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-27 03:51:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,1 @@
-var meals = { breakfast: "oatmeal", lunch: "turkey sandwich", dinner: "steak and potatoes" };
- 
-// the `delete` operator returns `true` if it has successfully
-// deleted, `false` otherwise
-console.log(delete meals.dinner); // true
- 
-console.log(meals)
-// returns { breakfast: "oatmeal", lunch: "turkey sandwich" }
+var playlist = {song:"mySweet"}
