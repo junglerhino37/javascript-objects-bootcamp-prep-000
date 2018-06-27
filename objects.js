943cc0db637e307ab4186c527a8b29ce38d43863
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-27 03:43:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ function updateObjectWithKeyAndValue(obj, key, value) {
  
 const recipe = { eggs: 3 }
  
-updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup')
+updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup');
 // returns `{ eggs: 3, chocolate: '1 cup' }`
  
 recipe // { eggs: 3 }
