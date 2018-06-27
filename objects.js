d30245819d0f0bb883280bb25e34c87fa411bbad
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-27 04:04:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist={Django:'My Sweet', Jimmy:"Undecided"}
 updatePlaylist(playlist,'Fappy','Lavende')
-
+removeFromPlaylist(Jimmy)
 console.log(playlist)
 
 function updatePlaylist(obj,key,value){
